df1e91858147e1c0a195c4293c1a744ce4aaf53b
gaurav-singh <stargaurav.singh0@gmail.com>
2022-08-31 12:20:20 +0530

    log

--- src/main/java/com/example/mongodb/MongodbApplication.java
+++ src/main/java/com/example/mongodb/MongodbApplication.java
@@ -1,10 +1,22 @@
 package com.example.mongodb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+
 @SpringBootApplication
 public class MongodbApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(SpringApplication.class);
+
+    @PostConstruct
+    void print(){
+        logger.info("Application started");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MongodbApplication.class, args);
